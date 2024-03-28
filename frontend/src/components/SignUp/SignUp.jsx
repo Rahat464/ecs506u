a15f7a4f6321985def3b4f4747ee159373315641
Zahid <zahid011001@gmail.com>
2024-03-28 02:05:51 +0000

    implemented some  routing, and created sign up form

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,13 +1,13 @@
-// login.jsx
+// SignUp.jsx
 import React from 'react';
-import './LoginForm.css';
+import './SignUp.css';
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import FDMLogo from '../../assets/FDMLogo.png'
 import { Link } from 'react-router-dom';
 
 
-export const LoginForm = () => {
+export const SignUp = () => {
   return (
     
     <div className='wrapper'>
@@ -16,25 +16,34 @@ export const LoginForm = () => {
       <div className="Logo">
             <img src={FDMLogo} alt="fdm logo" />
         </div>
-        <h1>Login</h1>
-        <p>Sign in to your account</p>
+        <h1>Sign Up</h1>
+        <p>Make a new account</p>
         <div className='input-box'>
             <input type="text" placeholder='Email' required />
             <FaUser className='icon'/>
+        </div>
+        <div className='Name'>
+            <div className='input-name'>
+               <input type="text" placeholder='First name' required />
+
+            </div>
+            <div className='input-name'>
+                <input type="text" placeholder='Last name' required />
+            </div>
         </div>
         <div className='input-box'>
             <input type="password" placeholder='Password' required />
             <FaLock className='icon' />
         </div>
 
-        <button type ="submit">Login</button>
+        <button type ="submit">Sign Up</button>
 
-        <div className="forgot-password">
-          <a href="#"> Forgot password?</a>
+        <div className="have-account">
+          <a href="#"> Already have an account?</a>
         </div>
 
-        <div className="register-link">
-          <Link to='/SignUp'>Sign Up</Link>
+        <div className="login-link">
+        <Link to='/LoginForm'>Login</Link>
         </div>
 
     
@@ -43,4 +52,4 @@ export const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default SignUp;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
 import './index.css'
+import SignUp from './components/SignUp/SignUp.jsx'
+import LoginForm from '../src/components/LoginForm/LoginForm.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>404 not found</div>,
+  },
+  {
+    path: '/SignUp',
+    element: <SignUp />,
+  },
+  {
+    path: '/LoginForm',
+    element: <LoginForm />,
+  },
+  
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
