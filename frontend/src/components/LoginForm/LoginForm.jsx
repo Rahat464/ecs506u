5d4f196761d4a54a31fe8a222a0076de31ebe10a
Alyosha Baryshnikov <alohahoy@gmail.com>
2024-03-29 20:28:35 +0000

    add a proxy to make requests from frontend to backend + login fetch request

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,6 @@
 // login.jsx
 import React from 'react';
+import { useState } from 'react';
 import './LoginForm.css';
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
@@ -9,27 +10,70 @@ import Header from '../header/header';
 
 
 export const LoginForm = () => {
+
+  // state for the email and password
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  // handle the call to the backend for logging in
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // call from proxy
+    fetch('/api/login/', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(
+        {
+          "email": email,
+          "password": password
+        }
+      )
+    }).then( () => {
+      console.log('Logging In.')
+    }).catch( () => {
+      console.log('Error Logging In.')
+    })
+  }
+
+
   return (
     <>
     <Header />
     <div className='Login'>
       <div className='shadow'> </div>
-      <form action="">
-      <div className="Logo">
-            <img src={FDMLogo} alt="fdm logo" />
+      <form onSubmit={ handleLogin }>
+        <div className="Logo">
+              <img src={FDMLogo} alt="fdm logo" />
         </div>
         <h1>Login</h1>
         <p>Sign in to your account</p>
         <div className='input-box'>
-            <input type="text" placeholder='Email' required />
+            <input 
+              type="text" 
+              placeholder='Email'
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+              required 
+            />
             <FaUser className='icon'/>
         </div>
         <div className='input-box'>
-            <input type="password" placeholder='Password' required />
+            <input 
+              type="password" 
+              placeholder='Password'
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
             <FaLock className='icon' />
         </div>
 
-        <button type ="submit">Login</button>
+        <button type="submit">Login</button>
 
         <div className="forgot-password">
           <a href="#"> Forgot password?</a>
@@ -38,8 +82,6 @@ export const LoginForm = () => {
         <div className="register-link">
           <Link to='/SignUp'>Sign Up</Link>
         </div>
-
-    
       </form>
     </div>
     </>
