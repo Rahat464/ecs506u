2e8a54b9f28ba80b79865eca3eea0876328edcba
Zahid <zahid011001@gmail.com>
2024-03-31 17:46:30 +0100

    implemented landing page, and tweaked logo position in header, not yet implemented picture from figma desgin

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -4,18 +4,28 @@ import FDMLogo from '../../assets/FDMLogo.png'
 import { Link } from 'react-router-dom';
 import Header from '../header/header';
 
-
 export const LandingPage = () => {
   return (
     <>
-        <Header />
-        <div className='LandingPage'>
-            <h1>this is the landing page (wip)</h1>
-            <ul>
-                <li><Link to = '/LoginForm'>Login</Link></li>
-                <li><Link to = '/SignUp'>Sign Up</Link></li>
-            </ul>
-        </div>
+      <Header />
+      <div className='Landing'>
+        <div className='shadow'></div>
+        <form>
+          <div className="Logo">
+            <img src={FDMLogo} alt="fdm logo"/>
+          </div>
+          <h1>Welcome to FDM employee portal</h1>
+
+          <div className="register-login">
+            <Link to='/SignUp'><button>Sign Up</button></Link>
+          </div>
+          <div className="register-login">
+            <Link to='/LoginForm'><button>Login</button></Link>
+          </div>
+
+          <p>An essential hub to access everything you’ll need during your time at FDM</p>
+        </form>
+      </div>
     </>
   );
 };
