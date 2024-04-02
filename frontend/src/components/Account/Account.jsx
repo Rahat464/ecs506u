a2ea4cdb734652778774c96f79bd71e55ec2242b
Zahid <zahid011001@gmail.com>
2024-04-02 03:20:04 +0100

    started account/edit account

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import './Account.css';
-import FDMLogo from '../../assets/FDMLogo.png'
-import { Link } from 'react-router-dom';
 import Header from '../header/header';
 
 
 export const Account = () => {
   return (
     <>
-        <Header />
-        <div className='Account'>
-            <h1>this is your account information</h1>
+        <div>
+          <Header />
         </div>
+        
+       
+        <div className='wrapper'> 
+        
+          <div className='Account-header'>
+            <h1>Personal information</h1>   
+          </div>
+        
+          <div className='info'>
+    
+          <div className="personal-info">
+            <h1 className='title'>First name</h1>
+          <div className='fname'>
+           <p>Zahid</p>
+          </div>
+            <h1 className='title '>Last name</h1>
+          <div className='lname'>
+          <p>Zahid</p>
+          </div>
+          <h1 className='title '>Number</h1>
+          <div className='number'>
+          <p>Zahid</p>
+          </div>
+          <h1 className='title '>Email</h1>
+          <div className='email'>
+          <p>Zahid</p>
+
+          </div>
+          <h1 className='title '>Password</h1>
+          <div className='password'>
+            <p>Zahid</p>
+          </div>
+          </div>
+          
+          </div>
+          
+          </div>
+
+        
     </>
   );
 };
--- frontend/src/components/Account/EditAccount.jsx
+++ frontend/src/components/Account/EditAccount.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
 import './EditAccount.css';
-import FDMLogo from '../../assets/FDMLogo.png'
 import { Link } from 'react-router-dom';
 import Header from '../header/header';
 
 
-export const EditAccount = () => {
+export const Account = () => {
   return (
     <>
-        <Header />
-        <div className='EditAccount'>
-            <h1>this is where you can edit your account information</h1>
+        <div>
+          <Header />
+        </div>
+        
+       
+        <div className='wrapper'> 
+        
+          <div className='Account-header'>
+            <h1>Update Personal information</h1>   
+          </div>
+          <div className='info'>
+            <h1 className='title'>First name</h1>
+          <div className='input-box'>
+          <input
+                type="text"
+                placeholder='First name'
+                required
+            />
+          </div>
+            <h1 className='title '>Last name</h1>
+          <div className='input-box'>
+          <input
+                type="text"
+                placeholder='Last name'
+                required
+            />
+          </div>
+          <h1 className='title '>Number</h1>
+          <div className='input-box'>
+          <input
+                type="text"
+                placeholder='First Name'
+                required
+            />
+          </div>
+          <h1 className='title '>Email</h1>
+          <div className='input-box'>
+          <input
+                type="email"
+                placeholder='Email'
+                required
+          />
+
+          </div>
+          <h1 className='title '>Password</h1>
+          <div className='input-box'>
+          <input
+                type="password"
+                placeholder='Password'
+                required
+            />
+          </div>
+          </div>
+
         </div>
     </>
   );
 };
 
-export default EditAccount;
+export default Account;
