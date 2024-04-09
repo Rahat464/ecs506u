dcecd29113f0fa019a71bdf42969ff1bbbc277eb
Syafiq Haikal Bin Syaidali <s.h.b.syaidali@se22.qmul.ac.uk>
2024-04-09 21:58:22 +0100

    Update and rename UploadDocuments.jsx to FileUploadForm.jsx

---
+++ frontend/src/components/Documents/FileUploadForm.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Header from '../header/header';
+import './FileUploadForm.css'; // Import the CSS file for styling
+
+const FileUploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [type, setType] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!file || !type) {
+      setErrorMessage('Please select a file and specify its type.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('type', type);
+
+    try {
+      await axios.post('/api/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      setFile(null);
+      setType('');
+      setErrorMessage('');
+      alert('File uploaded successfully!');
+    } catch (error) {
+      console.error('Error uploading file:', error.response ? error.response.data : error.message);
+      setErrorMessage('Error uploading file. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <Header/>
+      <div className='wrapper'>
+        <div className='upload-header'>
+          <h1>Upload Document</h1>
+        </div>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        <form onSubmit={handleSubmit}>
+        <div className='info'>
+          <div>
+            <label>Select File:</label>
+            <input type="file" onChange={handleFileChange} />
+          </div>
+          <div>
+            <label>Select Type:</label>
+            <select value={type} onChange={handleTypeChange}>
+            <option value="">Select Type</option>
+            <option value="progress_report">Progress Report</option>
+            <option value="payslip">Pay Slip</option>
+            <option value="other">Other</option>
+          </select>
+          </div>
+          <button type="submit">Upload</button>
+        </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FileUploadForm;
